463f57bc24d2520c20d855c08bbf45c908e9e782
Mayur Baravkar <mayurbaravkar24@gmail.com>
2025-07-20 20:21:58 +0530

    fixed env issue

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+import os 
 from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv 
+
+load_dotenv() 
 
 # MongoDB Atlas connection URI
-
 uri = os.getenv("MONGODB_URI")
 
 # Create a client and connect to the server
@@ -15,7 +18,7 @@ except Exception as e:
     print("Connection failed:", e)
 
 # Use database and collections
-db = client["hrone_db"]  # You can name it anything; this will be created on first insert
+db = client["hrone_db"]
 product_collection = db["products"]
 order_collection = db["orders"]
 
